7be35e82b5f4bd440bd31dc2eacd0e13a353aaa1
utakarsh23 <utakarsh.t@somaiya.edu>
2024-11-26 22:00:01 +0530

    lol

--- 11_binary.py
+++ 11_binary.py
@@ -2,3 +2,15 @@
 # Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and
 # then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in
 # a comma separated sequence.
+
+
+a = [int(input("Enter 4 digit binary no. ")), int(input("Enter 4 digit binary no. ")), int(input("Enter 4 digit binary no. ")), int(input("Enter 4 digit binary no. "))]
+
+li = []
+for i in range(len(a)):
+    if(a[i]%5 == 0):
+         li.append(a[i])
+         
+x = ','
+for i in li:
+    print(i, ", ", end='') #print(i, end=', ')
